94fb3e242d4a7bf3d8e679b6c0033d41a2d6171a
Shahar Gilaad <shaharorian@gmail.com>
2019-12-24 11:01:22 +0200

    Ready for productio

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 import _  from 'lodash'
-import { Input, Col } from 'antd';
-import { Button, InputGroup, FormControl, Container, Row } from 'react-bootstrap';
+import { FormControl, Container, Row, ToggleButton, ButtonGroup, InputGroup, Button } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -32,10 +31,17 @@ export default function App() {
   useEffect(() => {
     getImages()
   }, []);
-  function myFunction(){
+
+  function convertToGrey(){
     const imageByClass = document.getElementsByClassName("myImg")
     for(var i = 0; i < imageByClass.length; i++) {
       imageByClass[i].style.filter = "grayscale(100%)";
+    }
+  }
+  function convertToDefault(){
+    const imageByClass = document.getElementsByClassName("myImg")
+    for(var i = 0; i < imageByClass.length; i++) {
+      imageByClass[i].style.filter = "grayscale(0)";
     }
   }
 
@@ -45,7 +51,6 @@ export default function App() {
         <Row style={{ marginTop: '2em',  }}>
           <InputGroup className="mb-3 form-group w-50">
             <FormControl
-          
               aria-label="Default"
               aria-describedby="inputGroup-sizing-small"
               placeholder = 'Search for images by author name'
@@ -54,24 +59,31 @@ export default function App() {
           </InputGroup>
         </Row>
         <Row className="w-50" >
-          <Col style={{ float: 'none', margin: '0 auto' }}>
-            <Button variant="outline-dark" onClick={() => {getImages()}} style={{ margin: '0.3em' }}>Different Images</Button>
-            <Button  variant="dark" onClick={() => myFunction()} style={{ margin: '0.3em',  }}>Convert images into grayscale</Button>
-          </Col>
+          <Button variant="outline-dark" onClick={() => {getImages()}} style={{ margin: '0.3em' }}>Different Images</Button>
         </Row>
-        </Container>
-        <Container>
-         <div className="grid-container">
-            {filtered.map((image, ind) => {
-            return( 
+        <Row className="w-50" >
+          <ButtonGroup toggle className="mt-3">
+            <ToggleButton type="radio" name="radio"  value="1"  variant="dark" onClick={() => convertToGrey()} style={{ margin: '0.3em',  }}>
+                Convert images into grayscale
+            </ToggleButton>
+            <ToggleButton type="radio" name="radio" value="2" variant="outline-dark" onClick={() => convertToDefault()} style={{ margin: '0.3em',  }}>
+              Restore default 
+            </ToggleButton>
+          </ButtonGroup>
+        </Row>
+      </Container>
+      <Container>
+        <div className="grid-container">
+          {filtered.map((image, ind) => {
+          return( 
             <div className="grid-item" key={ind}>
               <img className="myImg" style={{ width:'300px', height: "300px" }} key={image.id} src={image.download_url}/>
               <p>{image.author}</p>
             </div>
-            )
-            })}
-          </div> 
-          </Container>
+          )
+          })}
+        </div> 
+      </Container>
     </div>
   );
 }
